7cb82c5b3747f03810bf20bc4e8257c6a94e8312
Дмитрий <dk.ionov@yande.ru>
2024-11-17 12:38:35 +0300

    Написал логику, но пока не работают некоторые запросы а именно (get(все запросы) и update

--- app/models/user.py
+++ app/models/user.py
@@ -14,6 +14,7 @@ class User(Base):
     age = Column(Integer)
     slug = Column(String, unique=True, index=True)
     tasks = relationship('Task', back_populates='user')
+    is_active = Column(Boolean, default=True)
 
 
 
--- app/routers/user.py
+++ app/routers/user.py
@@ -15,19 +15,19 @@ router = APIRouter(prefix="/user", tags=['user'])
 @router.get("/")
 async def all_users(db: Annotated[Session, Depends(get_db)]):
     users = db.scalars(select(User).where(User.is_active == True)).all()
-    return users
+    return list(users)
 
 
 @router.get("/{user_id}")
 async def user_by_id(user_id: int, db: Annotated[Session, Depends(get_db)]):
-    user = db.execute(select(User).where(User.is_active==True)).scalar().all()
-    if user is None:
+    user = db.scalars(select(User).where(User.user_id == user_id)).first()
+    if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User was not found")
     return user
 
 
 @router.post("/create")
-async def create_user(create_user: CreateUser, db: Session = Depends(get_db)):
+async def create_user(create_user: CreateUser, db: Annotated[Session, Depends(get_db)]):
     db.execute(insert(User).values(username=create_user.username,
     firstname=create_user.firstname,
     lastname=create_user.lastname,
@@ -41,14 +41,16 @@ async def create_user(create_user: CreateUser, db: Session = Depends(get_db)):
 
 @router.put("/update/{user_id}")
 async def user_by_id(user_id: int, update_user: UpdateUser, db: Annotated[Session, Depends(get_db)]):
-    user = db.scalar(select(User).where(User.id==user_id))
-    if user is None:
+    user = db.scalars(select(User).where(User.id==user_id)).first()
+    if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User was not found")
 
-    db.execute(update(User).where(User.id==user_id)).values(
-        firstname=update_user.firstname,
-        lastname=update_user.lastname,
-        age=update_user.age)
+    db.execute(update(User).where(User.id==user_id).values(
+            firstname=update_user.firstname if update_user.firstname else user.firstname,
+            lastname=update_user.lastname if update_user.lastname else user.lastname,
+            age=update_user.age if update_user.age else user.age
+        )
+    )
     db.commit()
 
     return {
@@ -59,8 +61,8 @@ async def user_by_id(user_id: int, update_user: UpdateUser, db: Annotated[Sessio
 
 @router.delete("/{user_id}")
 async def delete_user(user_id: int, db: Annotated[Session, Depends(get_db)]):
-    user = db.scalar(select(User).where(User.id==user_id))
-    if user is None:
+    user = db.scalars(select(User).where(User.id==user_id)).first()
+    if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
     db.execute(delete(User).where(User.id==user_id))
